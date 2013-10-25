d4d7dd4acc4e01c602ec4dfaec1b8b330839dd7b
simogeo <simon@linea21.com>
2013-10-25 12:19:22 +0200

    here we go

---
+++ sample/_link_to_pages.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * PHP sample
+ * You may want to query the database to populate the dedicated field
+ */
+
+session_start();
+header('Content-Type: text/html; charset=utf-8');
+
+// encodeURIComponent() is needed when working with accents
+// If not used, generate a JS error in CKEDITOR link plugin
+function encodeURIComponent( $str ){
+
+	$revert = array('%21'=>'!', '%2A'=>'*', '%27'=>"'", '%28'=>'(', '%29'=>')');
+	return strtr(rawurlencode($str), $revert);
+
+}
+
+
+$a = array();
+
+
+
+
+// Getting pages from your database
+///////////////////
+
+$data = $sql->getMyPages();
+
+foreach ($data as &$el) {
+
+	$name = encodeURIComponent($el['title']);
+	$link = 'index.php?p='.$el['id'];
+	
+	$tmp = array($name, $link);
+	array_push($a, $tmp);
+}
+
+// This is just a sample - this part of code as to be removed or commented
+echo "[['Contact','index.php?p=4'],['About','index.php?p=5'],['Home page','index.php?p=1']]";
+exit;
+
+// we finally encode the array into JSON
+$string = json_encode($a);
+echo $string;
+exit;
+?>
